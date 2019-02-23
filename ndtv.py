da55a44ce996b56205815900de56f7275a1dd77f
Compro-Prasad <comproprasad@gmail.com>
2019-02-24 01:22:01 +0530

    Added "content" and "timestamp" fields.
    Renamed:
    - "href"    -> "link"
    - "content" -> "title"
    
    get_headlines now takes url instead of html content.
    
    - modified:   ndtv.py
    - modified:   the-hindu.py

--- ndtv.py
+++ ndtv.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import requests
+from bs4 import BeautifulSoup
 
 def is_string(obj):
     return isinstance(obj, str)
@@ -13,42 +15,45 @@ def str_is_set(attr):
     return attr and attr != ""
 
 
+def get_content(url):
+    return "NA"
+
+
 def get_headline_details(obj):
     try:
+        from datetime import datetime
         return {
-            "href": obj["href"],
-            "content": obj["title"]
+            "content": get_content(obj["href"]),
+            "link": obj["href"],
+            "timestamp": str(datetime.utcnow()),
+            "title": obj["title"]
         }
     except KeyError:
         import pdb
         pdb.set_trace()
 
 
-def get_headlines(text):
-    soup = BeautifulSoup(text, 'html.parser')
-    headline_tags = list(map( lambda x : x.find("a"), soup.find_all("div", {
-        "class": "new_storylising_img"
-    })))
+def get_headlines(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        headline_tags = list(map( lambda x : x.find("a"), soup.find_all("div", {
+            "class": "new_storylising_img"
+        })))
 
-    # It returns 15 results, we are taking just 10 results.
-    return list(map(get_headline_details, headline_tags))[:10]
+        # It returns 15 results, we are taking just 10 results.
+        return list(map(get_headline_details, headline_tags))[:10]
+    return None
 
 
 if __name__ == "__main__":
-    import requests
     import json
-    from bs4 import BeautifulSoup
 
     # Don't change this unknowingly
     NEWS_WEBSITE = "https://www.ndtv.com/india"
 
-    response = requests.get(NEWS_WEBSITE)
-
-    if (response.status_code == 200):
-        print(json.dumps(
-            get_headlines(response.text),
-            sort_keys=True,
-            indent=4)
-        )
-    else:
-        print("Error")
+    print(json.dumps(
+        get_headlines(NEWS_WEBSITE),
+        sort_keys=True,
+        indent=4
+    ))
--- the-hindu.py
+++ the-hindu.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import requests
+from bs4 import BeautifulSoup
 
 def is_string(obj):
     return isinstance(obj, str)
@@ -13,11 +15,32 @@ def str_is_set(attr):
     return attr and attr != ""
 
 
+def get_content(url):
+    import re
+    response = requests.get(url)
+    if response.status_code == 200:
+        html_content = BeautifulSoup(response.text, 'html.parser')
+        text = html_content.find("div", {
+            "class": "article"
+        }).find("div", id=re.compile('content-body*')).get_text()
+        return text
+    return "NA"
+
+
+i = 0
 def get_headline_details(obj):
     try:
+        from datetime import datetime
+        from time import sleep
+        sleep(1)
+        global i
+        print("Downloading " + str(i) + " article")
+        i += 1
         return {
-            "href": obj["href"],
-            "content": "\n".join(filter(
+            "content": get_content(obj["href"]),
+            "link": obj["href"],
+            "timestamp": str(datetime.utcnow()),
+            "title": "\n".join(filter(
                 str_is_set,
                 map(
                     strip_string,
@@ -30,30 +53,26 @@ def get_headline_details(obj):
         pdb.set_trace()
 
 
-def get_headlines(text):
-    soup = BeautifulSoup(text, 'html.parser')
-    main_div = soup.find("div", {
-        "class": "justin-text-cont"
-    })
-    headline_tags = main_div.find_all("a", href=str_is_set)
-    return list(map(get_headline_details, headline_tags))[:10]
+def get_headlines(url):
+    response = requests.get(url)
+    if (response.status_code == 200):
+        soup = BeautifulSoup(response.text, 'html.parser')
+        main_div = soup.find("div", {
+            "class": "justin-text-cont"
+        })
+        headline_tags = main_div.find_all("a", href=str_is_set)
+        return list(map(get_headline_details, headline_tags))[:10]
+    return None
 
 
 if __name__ == "__main__":
-    import requests
     import json
-    from bs4 import BeautifulSoup
 
     # Don't change this unknowingly
     NEWS_WEBSITE = "https://www.thehindu.com/"
 
-    response = requests.get(NEWS_WEBSITE)
-
-    if (response.status_code == 200):
-        print(json.dumps(
-            get_headlines(response.text),
-            sort_keys=True,
-            indent=4)
-        )
-    else:
-        print("Error")
+    print(json.dumps(
+        get_headlines(NEWS_WEBSITE),
+        sort_keys=True,
+        indent=4
+    ))
