378953dd184d15389ee261155e04e66956b663db
Compro-Prasad <comproprasad@gmail.com>
2019-03-01 01:31:18 +0530

    Move scripts that scrape headlines to the scraper directory

--- update-db.py
+++ update-db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from settings import new_connection
+from scraper.settings import new_connection
 from importlib import import_module
 
 
@@ -27,7 +27,7 @@ def update_database(collection, headlines):
 
 if __name__ == "__main__":
     for src in KNOWN_NEWS_SOURCES:
-        src["module"] = src["channel"].lower().replace(" ", "-")
+        src["module"] = "scraper." + src["channel"].lower().replace(" ", "-")
         mod = import_module(src["module"])
         headlines = mod.get_headlines(src["link"])
         update_database(src["channel"], headlines)
